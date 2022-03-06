917ec8563d72b8065f2fb4cd875750f016fc7537
krispin <krispinblow@gmail.com>
2022-03-06 09:27:25 -0600

    working model

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ class TeacherchoiceSimulation:
     
     def get_teacher_choice_1(self):
         choice_number = int(input('When will the meetings be held?\nEnter the number of your choice: '))
-        self.teacher_choice = OPTIONS1[choice_number]
+        self.teacher_choice = OPTIONS1[choice_numer]
     
     def print_options(self):
         print('\n'.join(f'({i}) {option.title()}' for i, option in enumerate(OPTIONS1)))
@@ -59,10 +59,63 @@ class Decision:
  
     print(tabulate(decisions, headers=col_names, tablefmt="grid", showindex="no"))'''
     
-#OPTIONS1 = ['during class time', 'before/after school or during non-instructional time'] 
+OPTIONS1 = ['during class time', 'before/after school or during non-instructional time'] 
+OPTIONS2 = ['Elementary', 'Secondary']
  
 class Questions:
-    def __init__(self):
-        self.teacher_choice = None
-    
-    
+    while True:
+        qid1 = input("When will the meetings be held? \n 1) during class time \n 2) before/after school or non-instructional time \n: ")
+        if qid1 == "1":
+            print(Decision.decision1)
+            break
+        elif qid1 == "2":
+            print("This may be a student activity. Please continue...") 
+            
+        
+        qid2 = input("Elementary or Secondary Level? \n 1) Elementary \n 2) Secondary \n: ")
+        if qid2 == "1":
+            qid3 = input("STUCO? \n 1) Yes \n 2) No \n:")
+            if qid3 == "1":
+                print(Decision.decision2)
+                break
+            elif qid3 == "2":
+               print(Decision.decision1)
+               break
+        elif qid2 == "2":
+             print("This may be a student activity. Please continue...")
+        
+        qid4 = input("Who runs the meetings/makes decisions? \n 1) Sponsor/Teacher \n 2) Students/Members \n: ")
+        if qid4 == "1":
+             print(Decision.decision1)
+             break
+        elif qid4 == "2":
+            print("This may be a student activity. Please continue...")
+        
+        qid5 = input("Documented minutes of each meeting? \n 1) Yes \n 2) No \n: ")
+        if qid5 == "1":
+            qid6 = input("Vote for Officers? \n 1) Yes \n 2) No \n: ")
+            if qid6 == "1":
+                qid7 = input("Monetary transactions? \n 1) Yes \n 2) No \n: ")
+                if qid7 == "1":
+                    qid8 = input("Open to the student body? \n 1) Yes \n 2) No \n: ")
+                    if qid8 == "1":
+                        qid9 = input("Curriculum based? \n 1) Yes \n 2) No \n: ")
+                        if qid9 == "1":
+                            print(Decision.decision2)
+                            break
+                        elif qid9 == "2":
+                            print(Decision.decision4)
+                            break
+                    elif qid8 == "2":
+                        print(Decision.decision1)
+                        break
+                elif qid7 == "2":
+                    print(Decision.decision3)
+                    break
+            elif qid6 == "2":
+                print(Decision.decision1)
+                break
+        elif qid5 == "2":
+            print(Decision.decision1)
+            break
+           
--- orm.py
+++
@@ -1,4 +0,0 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-
