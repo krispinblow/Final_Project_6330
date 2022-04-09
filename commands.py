c4d8a6d478049b7f18108ec66797641887f5962a
krispin <krispinblow@gmail.com>
2022-04-09 09:19:40 -0500

    correcting code

---
+++ commands.py
@@ -0,0 +1,33 @@
+import sys
+from datetime import datetime
+
+from database import DatabaseManager
+
+db = DatabaseManager('evaluates.db')  # <1>
+
+
+class CreateEvaluateTableCommand:
+    def execute(self):  # <2>
+        db.create_table('evaluates', {  # <3>
+            'id': 'integer primary key autoincrement',
+            'name': 'text not null',
+            'date_added': 'text not null',
+        })
+
+
+class CampusActivityCommand:
+    def execute(self, data):
+        data['date_added'] = datetime.utcnow().isoformat()  # <1>
+        db.add('evaluates', data)  # <2>
+        return 'You chose during classtime. This is a campus activity!'  # <3>
+                     
+
+class StudentActivityCommand:
+     def execute(self, data):
+        data['date_added'] = datetime.utcnow().isoformat()  # <1>
+        db.add('evaluates', data)  # <2>
+        return 'You chose after school. This is a student activity!'  # <3>
+    
+class QuitCommand:
+    def execute(self):
+        sys.exit()  # <1>  
--- model.py
+++ model.py
@@ -1,3 +1,114 @@
+#!/usr/bin/env python
+
+import os
+from collections import OrderedDict
+
+import commands
+
+
+def print_evaluations(evaluations):
+    for evaluate in evaluations:
+        print('\t'.join(
+            str(field) if field else ''
+            for field in evaluate
+        ))
+
+
+class Question:
+    def __init__(self, name, command, prep_call=None):
+        self.name = name  # <1>
+        self.command = command  # <2>
+        self.prep_call = prep_call  # <3>
+
+    def _handle_message(self, message):
+        if isinstance(message, list):
+            print_evaluations(message)
+        else:
+            print(message)
+
+    def choose(self):  # <4>
+        data = self.prep_call() if self.prep_call else None  # <5>
+        message = self.command.execute(data) if data else self.command.execute()  # <6>
+        self._handle_message(message)
+
+    def __str__(self):  # <7>
+        return self.name
+
+
+def clear_screen():
+    clear = 'cls' if os.name == 'nt' else 'clear'
+    os.system(clear)
+
+
+def print_questions(questions):
+    for shortcut, question in questions.items():
+        print(f'({shortcut}) {question}')
+    print()
+
+
+def question_choice_is_valid(choice, questions):
+    return choice in questions or choice.upper() in questions  # <1>
+
+
+def get_question_choice(questions):
+    choice = input('Choose an option: ')  # <2>
+    while not question_choice_is_valid(choice, questions):  # <3>
+        print('Invalid choice')
+        choice = input('Choose an option: ')
+    return questions[choice.upper()]  # <4>
+
+
+def get_user_input(label, required=True):  # <1>
+    value = input(f'{label}: ') or None  # <2>
+    while required and not value:  # <3>
+        value = input(f'{label}: ') or None
+    return value
+
+
+def get_new_evaluate_data():  # <4>
+    return {
+        'name': get_user_input('Name'),
+    }
+
+'''
+def get_bookmark_id_for_deletion():  # <6>
+    return get_user_input('Enter a bookmark ID to delete')'''
+
+
+def loop():  # <1>
+    clear_screen()
+
+    questions = OrderedDict({
+        'A': Question('When will meetings be held?  During class?', commands.CampusActivityCommand(), prep_call=get_new_evaluate_data),
+        'B': Question('When will meetings be held? After School?', commands.StudentActivityCommand(), prep_call=get_new_evaluate_data),
+        'Q': Question('Quit', commands.QuitCommand()),
+    })
+    print_questions(questions)
+
+    chosen_question = get_question_choice(questions)
+    clear_screen()
+    chosen_question.choose()
+
+    _ = input('Press ENTER to return to menu')  # <2>
+
+
+if __name__ == '__main__':
+    commands.CreateEvaluateTableCommand().execute()
+
+    while True:  # <3>
+        loop()
+
+
+def for_listings_only():
+    questions = {
+        'A': Question('When will meetings be held?  During class?', commands.AddEvaluateCommand()),
+        'B': Question('When will meetings be held? After School?', commands.ListEvaluatesCommand()),
+        'Q': Question('Quit', commands.QuitCommand()),
+    }
+    print_questions(questions)
+
+
+'''
 from __future__ import annotations
 from dataclasses import dataclass
 from typing import Optional, List, Set
@@ -18,12 +129,12 @@ class DecisionLine:
     dec3 = "Not an Activity Fund"
   
 
-class Question:
+class evaluate:
     qid1: str
     qid2: str
     
    
-    '''def eval_input(self, qid1, qid2, dec1, dec2, dec3, aid1, aid2):
+    def eval_input(self, qid1, qid2, dec1, dec2, dec3, aid1, aid2):
         if qid1 == aid2:
             print(qid2)
             if qid2 == aid1:
@@ -31,7 +142,7 @@ class Question:
             elif qid2 == aid2:
                 return(dec3)
         elif qid1 == aid1:
-            return(dec1)'''
+            return(dec1)
     
 while True:
     qid1 = input(f'When are meetings held? {choices}:')
@@ -45,7 +156,7 @@ while True:
             break
         elif qid2 == "B":
             print(DecisionLine.dec3)
-            break 
+            break '''
     
 
     
